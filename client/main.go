19c9306f4b8d5d3e0432d9e3a3d2bfd00699a5d6
ssr0016 <samsonrecluta0016@gmail.com>
2024-04-20 16:55:39 +0800

    grpc test

--- client/main.go
+++ client/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	pb "grpc/proto"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":9000"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	// names := &pb.NamesList{
+	// 	Names: []string{
+	// 		"John",
+	// 		"Jane",
+	// 	},
+	// }
+
+	callSayHello(client)
+
+}
